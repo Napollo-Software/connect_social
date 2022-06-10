50673a4cf374528f1751397cd83db9379624efce
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-06-10 23:13:39 +0500

    main

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\HomeController;
 
 Auth::routes();
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-// INSERT INTO `users` (`id`, `name`, `role`, `email`, `password`, `created_at`, `updated_at`) VALUES
-//(1, 'Administrator', 1, 'super_admin@connect.com', '$2y$10$D4qxG3pfH3FkDbbkyGKRi.qdGdi0DcwKlqnnLKB6jtdW.c2fGj/.G', '2022-06-10 16:24:09', '2022-06-10 16:24:09');
+// INSERT INTO `users` (`id`, `name`, `role`, `email`, `password`, `created_at`, `updated_at`) VALUES (1, 'Administrator', 1, 'super_admin@connect.com', '$2y$10$D4qxG3pfH3FkDbbkyGKRi.qdGdi0DcwKlqnnLKB6jtdW.c2fGj/.G', '2022-06-10 16:24:09', '2022-06-10 16:24:09');
