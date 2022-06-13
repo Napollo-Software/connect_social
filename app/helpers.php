b724710b179ae3c2966553db958808c494a5106f
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-06-13 23:28:22 +0500

    php mailer

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,36 @@
 <?php
+use \PHPMailer\PHPMailer\PHPMailer;
+function sendEmail($to,$from,$subject,$message){
+    $mail = new PHPMailer(true);
+    try {
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.example.com';             //  smtp host
+        $mail->SMTPAuth = true;
+        $mail->Username = 'connectsocial@napollo.net';   //  sender username
+        $mail->Password = 'SDE$#@W#42';       // sender password
+        $mail->SMTPSecure = 'tls';                  // encryption - ssl/tls
+        $mail->Port = 587;                          // port - 587/465
+
+        $mail->setFrom('connectsocial@napollo.net', 'Connect social');
+        $mail->addAddress($to);
+        //$mail->addCC();
+        //$mail->addBCC($request->emailBcc);
+        //$mail->addReplyTo('sender@example.com', 'SenderReplyName');
+
+        $mail->isHTML(true);                // Set email content format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+        // $mail->AltBody = plain text version of email body;
+        if( !$mail->send() ) {
+            return back()->with("failed", "Email not sent.")->withErrors($mail->ErrorInfo);
+        }
+
+        else {
+            return back()->with("success", "Email has been sent.");
+        }
+
+    } catch (Exception $e) {
+        return back()->with('error','Message could not be sent.');
+    }
+}
