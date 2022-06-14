cbdd39a7ad861d6d64a9755588f1d4c430098f90
sharozmalik <107432076+sharozmalik@users.noreply.github.com>
2022-06-14 10:20:12 -0700

    m

--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -8,5 +8,6 @@ Route::middleware('auth')->group(function () {
     Route::prefix('invite')->group(function () {
         Route::get('/', [InviteController::class, 'index'])->name('invite');
         Route::post('send', [InviteController::class, 'send'])->name('invite.send');
+       
     });
 });
