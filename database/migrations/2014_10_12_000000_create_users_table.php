c8b0d856eb8ff17d50d8650b840dd4c6033e5429
sharozmalik <107432076+sharozmalik@users.noreply.github.com>
2022-06-16 04:18:07 -0700

    m

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,7 +25,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->string('gender');
             $table->integer('role');
-
             $table->string('email_code')->nullable();
             $table->string('phone_code')->nullable();
 
