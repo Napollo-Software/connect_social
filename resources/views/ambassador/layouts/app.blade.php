13d0a840a26c2b4a914c801393e676384ad3bc41
MIRFAN1N <107563495+MIRFAN1N@users.noreply.github.com>
2022-07-01 00:34:14 +0500

    Pop-Up Functionalities Updated

--- resources/views/ambassador/layouts/app.blade.php
+++ resources/views/ambassador/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="{{url('ambassador_assets/lib/tags/tags.css')}}">
     <link rel="stylesheet" href="{{url('ambassador_assets/lib/tags/tagsify.css')}}">
 </head>
-<body class="show-post">
+<body>
 <div class="page-wrapper">
     <div class="page-wrapper-inner">
         <!-- <div class="container"> -->
