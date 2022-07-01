3b2ca7897053efe82f955e0d0f947f5c05dde2b3
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-07-01 20:31:48 +0500

    posts

--- app/Http/Controllers/Ambassador/PostController.php
+++ app/Http/Controllers/Ambassador/PostController.php
@@ -21,26 +21,24 @@ class PostController extends Controller
     public function fetch_all(Request $request){
         $type=$request->type;
         if ($type=='all'){
-            $posts = Post::orderBy('created_at', 'DESC')->where('user_id',auth()->user()->my_network())->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',auth()->user()->my_network())->skip($request->n*2)->take(2)->get();
         }
         if ($type=='friends'){
-            $friends=getArrayFromKeyofEloquent(getFriendsListUsers(auth()->user()->ud),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->where('user_id',$friends)->skip($request->n*2)->take(2)->get();
+            $friends=getArrayFromKeyofEloquent(getFriendsListUsers(auth()->user()->id),'id');
+            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$friends)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='connections'){
-            $connections=getArrayFromKeyofEloquent(getConnectionsListUsers(auth()->user()->ud),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->where('user_id',$connections)->skip($request->n*2)->take(2)->get();
+            $connections=getArrayFromKeyofEloquent(getConnectionsListUsers(auth()->user()->id),'id');
+            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$connections)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='tier-1'){
             $tier1=getArrayFromKeyofEloquent(auth()->user()->tier_1(),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->where('user_id',$tier1)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$tier1)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='tier-2'){
             $tier1=getArrayFromKeyofEloquent(auth()->user()->tier_2(),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->where('user_id',$tier1)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$tier1)->skip($request->n*2)->take(2)->get();
         }
-
-
         $user=User::find($request->user);
         $viewRender = view('ambassador.profile.components.partial.posts_html',compact('posts','user'))->render();
         return response()->json($viewRender);
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -14,6 +15,15 @@ class HomeController extends Controller
     }
     public function index($type=null)
     {
+        /*$class = new \ReflectionClass('Privacy');
+        foreach (User::where('role',3)->get() as $user){
+            foreach ($class->getConstants() as $constant){
+                $sql="INSERT INTO `posts` ( `user_id`, `details`, `privacy`, `deleted_at`, `created_at`, `updated_at`) VALUES
+            ( ".$user->id.", 'Testing post from ".$user->fullName()." (".$user->email.") with privacy of ".$constant."', '".$constant."', NULL, '2022-07-0".rand(1,9)." 10:12:10', '2022-07-01 10:12:10');";
+                echo '<br>'.$sql;
+            }
+        }
+        dd(1);*/
         if (Auth::user()->roles->slug == 'ambassador') {
             $user = auth()->user();
             return view('ambassador.home.index', compact( 'user'));
