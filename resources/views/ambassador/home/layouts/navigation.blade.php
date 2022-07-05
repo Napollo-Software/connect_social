57192063fe5abe95a08de85731a222782833ba01
MIRFAN1N <107563495+MIRFAN1N@users.noreply.github.com>
2022-07-06 00:07:59 +0500

    Update navigation.blade.php

--- resources/views/ambassador/home/layouts/navigation.blade.php
+++ resources/views/ambassador/home/layouts/navigation.blade.php
@@ -8,7 +8,7 @@
                     </div>
                 </div>
                 <ul class="home-navigation-ul">
-                <li class="home-navigation-li {{Route::getCurrentRoute()->type=='friends'?'active':''}}"><a href="{{url('home/friends')}}" class="home-navigation-link"><span class="icon"><img src="{{asset('ambassador_assets/images/icons/users.svg')}}" alt=""></span> <span class="text">Everyone</span></a></li>
+                <li class="home-navigation-li {{Route::getCurrentRoute()->type=='friends'?'active':''}}"><a href="{{url('home/friends')}}" class="home-navigation-link"><span class="icon"><img src="{{asset('ambassador_assets/images/icons/globe.svg')}}" alt=""></span> <span class="text">Everyone</span></a></li>
                     <li class="home-navigation-li {{Route::getCurrentRoute()->type=='friends'?'active':''}}"><a href="{{url('home/friends')}}" class="home-navigation-link"><span class="icon"><img src="{{asset('ambassador_assets/images/icons/users.svg')}}" alt=""></span> <span class="text">Friends</span></a></li>
                     <li class="home-navigation-li {{Route::getCurrentRoute()->type=='connections'?'active':''}}"><a href="{{url('home/connections')}}" class="home-navigation-link"><span class="icon"><img src="{{asset('ambassador_assets/images/icons/connection.svg')}}" alt=""></span> <span class="text">Connections</span></a></li>
                     <li class="home-navigation-li"><a href="javascript:void(0)" class="home-navigation-link"><span class="icon"><img src="{{asset('ambassador_assets/images/icons/personal-network.svg')}}" alt=""></span> <span class="text">Personal Network</span></a></li>
