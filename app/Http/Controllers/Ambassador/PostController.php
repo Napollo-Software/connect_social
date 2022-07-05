080c16dcc31b3e3e8097eb310d3e526402aeeb0e
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-07-05 23:39:26 +0500

    main

--- app/Http/Controllers/Ambassador/PostController.php
+++ app/Http/Controllers/Ambassador/PostController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Storage;
 class PostController extends Controller
 {
     public function fetch(Request $request){
-        $posts = Post::where('user_id', auth()->user()->id)->orderBy('created_at', 'DESC')->skip($request->n*2)->take(2)->get();
+        $posts = Post::where('user_id', $request->user)->orderBy('created_at', 'DESC')->skip($request->n*2)->take(2)->get();
         $user=User::find($request->user);
         $viewRender = view('ambassador.profile.components.partial.posts_html',compact('posts','user'))->render();
         return response()->json($viewRender);
