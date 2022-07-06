cf8c5fd9ab49a3bcecd145cedbb93756be43e066
usamafiaz <usama.fiaz@napollo.net>
2022-07-06 05:59:57 -0700

    privacy

--- app/Http/Controllers/Ambassador/PostController.php
+++ app/Http/Controllers/Ambassador/PostController.php
@@ -21,25 +21,26 @@ class PostController extends Controller
     public function fetch_all(Request $request){
         
         $type=$request->type;
+        $posts = Post::orderBy('created_at', 'DESC')->where('privacy','public')->whereIn('user_id',auth()->user()->my_network())->skip($request->n*2)->take(2)->get();
         if ($type=='all'){
-            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',auth()->user()->my_network())->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->where('privacy','public')->whereIn('user_id',auth()->user()->my_network())->skip($request->n*2)->take(2)->get();
         }
         if ($type=='friends'){
             $friends=getArrayFromKeyofEloquent(getFriendsListUsers(auth()->user()->id),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$friends)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->where('privacy','friends')->whereIn('user_id',$friends)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='connections'){
             
             $connections=getArrayFromKeyofEloquent(getConnectionsListUsers(auth()->user()->id),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$connections)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->where('privacy','connections')->whereIn('user_id',$connections)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='tier-1'){
             $tier1=getArrayFromKeyofEloquent(auth()->user()->tier_1(),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$tier1)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->where('privacy','tier-1')->whereIn('user_id',$tier1)->skip($request->n*2)->take(2)->get();
         }
         if ($type=='tier-2'){
             $tier2=getArrayFromKeyofEloquent(auth()->user()->tier_2(),'id');
-            $posts = Post::orderBy('created_at', 'DESC')->whereIn('user_id',$tier2)->skip($request->n*2)->take(2)->get();
+            $posts = Post::orderBy('created_at', 'DESC')->where('privacy','tier-2')->whereIn('user_id',$tier2)->skip($request->n*2)->take(2)->get();
         }
         $user=User::find($request->user);
         $viewRender = view('ambassador.profile.components.partial.posts_html',compact('posts','user'))->render();
