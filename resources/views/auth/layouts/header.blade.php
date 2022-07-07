18008dfa7a62ffa44cb40bb91309d0be1b027a08
MIRFAN1N <107563495+MIRFAN1N@users.noreply.github.com>
2022-07-07 19:20:57 +0500

    Updated

--- resources/views/auth/layouts/header.blade.php
+++ resources/views/auth/layouts/header.blade.php
@@ -1,4 +1,4 @@
-<div class="action-div p-3 hide-on-mobile">
+<!-- <div class="action-div p-3 hide-on-mobile">
     <div class="action-div-inner">
         <div class="containetr">
             <div class="row justify-content-center">
@@ -10,4 +10,4 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
--- resources/views/join_as.blade.php
+++ resources/views/join_as.blade.php
@@ -1,5 +1,18 @@
 @extends('auth.layouts.app')
 @section('content')
+<div class="action-div p-3 hide-on-mobile">
+    <div class="action-div-inner">
+        <div class="containetr">
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-sm-8 col-xs-12">
+                    <div class="action-div-logo-inner">
+                        <img src="{{url('assets/images/logo.png')}}" alt="" class="w-100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
     <link rel="stylesheet" href="{{url('assets/css/all.css')}}">
     <div class="page-navigation">
         <div class="container">
