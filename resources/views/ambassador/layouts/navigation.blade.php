d43c87a0ae7c610b110e9799fc3dafd39cb1463b
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-07-19 19:19:55 +0500

    main

--- resources/views/ambassador/layouts/navigation.blade.php
+++ resources/views/ambassador/layouts/navigation.blade.php
@@ -2,24 +2,32 @@
 	<div class="container">
 		<div class="profile-site-navigation-inner">
 			<ul class="profile-site-navigation-ul">
-                <li class="profile-site-navigation-li {{(Route::currentRouteName()=='home')?'active':''}}">
+                {{--<li class="profile-site-navigation-li {{(Route::currentRouteName()=='home')?'active':''}}">
 					<a href="{{route('home')}}" class="profile-site-navigation-link"> <span class="icon">
                     <img src="{{url('ambassador_assets/images/nav-icon/home.png')}}" alt="">
                     </span> <span class="text">Timeline</span> </a>
 				</li>
+				--}}
 				<li class="profile-site-navigation-li {{(Route::currentRouteName()=='ambassador.home')?'active':''}}">
 					<a href="javascript:void(0)" class="profile-site-navigation-link"> <span class="icon">
                <img src="{{url('ambassador_assets/images/nav-icon/connection.png')}}" alt="">
-               </span> <span class="text">Social</span> </a>
+               </span> <span class="text">Timeline</span> </a>
 					<div class="navigation-drop-down">
 						<div class="navigation-drop-down-inner">
 							<ul class="navigation-drop-down-">
+								<li class="navigation-drop-down-li {{Route::getCurrentRoute()->type=='friends'?'active':''}}">
+									<a href="{{route('ambassador.home',['friends'])}}" class="navigation-drop-down-link">
+										<div class="icon"><img src="{{asset('ambassador_assets/images/icons/globe.svg')}}" alt=""> </div>
+										<div class="text">Public</div>
+									</a>
+								</li>
 								<li class="navigation-drop-down-li {{Route::getCurrentRoute()->type=='friends'?'active':''}}">
 									<a href="{{route('ambassador.home',['friends'])}}" class="navigation-drop-down-link">
 										<div class="icon"><img src="{{asset('ambassador_assets/images/icons/users.svg')}}" alt=""> </div>
 										<div class="text">Friends</div>
 									</a>
 								</li>
+
 								<li class="navigation-drop-down-li">
 									<a href="{{route('ambassador.home',['connections'])}}" class="navigation-drop-down-link">
 										<div class="icon"><img src="{{asset('ambassador_assets/images/icons/connection.svg')}}" alt=""></div>
