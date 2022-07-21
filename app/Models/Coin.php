787c3b3aa90ffc0b186a12ee09ecc50a3c004f27
Napollo-Software <105448336+Napollo-Software@users.noreply.github.com>
2022-07-22 00:49:38 +0500

    main

--- app/Models/Coin.php
+++ app/Models/Coin.php
@@ -10,7 +10,24 @@ class Coin extends Model
     use HasFactory;
     protected $table="coin";
     public static function CurrentValue(){
-        $latest=Coin::latest()->first();
+        $latest=Coin::orderBy('created_at', 'desc')->first();
         return $latest?$latest->dollar:0;
+    }
+    public static function differenceFromCurrent(){
+        $secondLast=Coin::orderBy('created_at', 'desc')->skip(1)->first();
+        $difference=self::CurrentValue()-$secondLast->dollar;
+        return $secondLast?$difference:0;
+    }
+    public static function balanceOfCurrentUser(){
+        $my_trxs=JournalDetails::where('account',auth()->user()->coa)->get();
+        $balance=0;
+        foreach ($my_trxs as $my_trx) {
+            if ($my_trx->dr){
+                $balance+=$my_trx->dr;
+            }else{
+                $balance-=$my_trx->dr;
+            }
+        }
+        return $balance;
     }
 }
